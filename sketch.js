c6b1c53dd794047707c8a3509ba372552a036d05
AS1n <novruzov.aslan.as@gmail.com>
2018-08-10 20:46:28 +0300

    First version

---
+++ sketch.js
@@ -0,0 +1,218 @@
+let grid;
+var score = 0;
+
+function blankGrid() {
+  return [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ];
+}
+
+function setup() {
+  noLoop();
+  createCanvas(400, 400);
+  grid = blankGrid();
+  addNumber();
+  addNumber();
+  updateCanvas();
+  //  console.table(grid);
+}
+
+function addNumber() {
+  let options = [];
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (grid[i][j] === 0)
+        options.push({
+          x: i,
+          y: j
+        });
+    }
+  }
+
+  if (options.length > 0) {
+    let spot = random(options);
+    let r = random(1);
+    grid[spot.x][spot.y] = r > 0.5 ? 2 : 4;
+    //    updateScore(grid[spot.x][spot.y]);
+  }
+
+}
+
+function updateScore(num) {
+  score += num;
+  document.querySelector('#score').textContent = score;
+}
+
+//  Some functions
+
+function copyGrid(grid) {
+  let extra = blankGrid();
+
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      extra[i][j] = grid[i][j];
+    }
+  }
+
+  return extra;
+}
+
+function compare(a, b) {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (a[i][j] !== b[i][j])
+        return true;
+    }
+  }
+  return false;
+}
+
+function flipGrid(grid) {
+  for (let i = 0; i < 4; i++) {
+    grid[i].reverse();
+  }
+  return grid;
+}
+
+function rotateGrid(grid) {
+  let newGrid = blankGrid();
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      newGrid[i][j] = grid[j][i];
+    }
+  }
+  return newGrid;
+}
+
+// one "move"
+function keyPressed() {
+  //  console.log(keyCode);
+  let played = true;
+  let flipped = false;
+  let rotated = false;
+  let tripleRotated = false;
+  if (keyCode === DOWN_ARROW) {
+    //DO NOTHING
+  } else if (keyCode === UP_ARROW) {
+    grid = flipGrid(grid);
+    flipped = true;
+  } else if (keyCode === RIGHT_ARROW) {
+    grid = rotateGrid(grid);
+    rotated = true;
+  } else if (keyCode === LEFT_ARROW) {
+    grid = rotateGrid(grid);
+    grid = flipGrid(grid);
+    rotated = true;
+    flipped = true;
+  } else {
+    played = false;
+  }
+
+  if (played) {
+
+    let past = copyGrid(grid);
+    for (let i = 0; i < 4; i++) {
+      grid[i] = operate(grid[i]);
+    }
+
+    let changed = compare(past, grid);
+
+    if (flipped) {
+      flipGrid(grid);
+    }
+
+    if (rotated) {
+      grid = rotateGrid(grid);
+      grid = rotateGrid(grid);
+      grid = rotateGrid(grid);
+    }
+
+
+    if (changed) {
+      addNumber();
+    }
+    updateCanvas();
+    
+    if(isGameOver())
+      console.log('GAME OVER!');
+  }
+}
+
+function operate(row) {
+  row = slide(row);
+  row = combine(row);
+  row = slide(row);
+  return row;
+}
+
+function slide(row) {
+  let arr = row.filter(val => val);
+  let missing = 4 - arr.length;
+  let zeros = Array(missing).fill(0);
+  arr = zeros.concat(arr);
+  return arr;
+}
+
+function combine(row) {
+  for (let i = 3; i >= 1; i--) {
+    let a = row[i];
+    let b = row[i - 1];
+    if (a === b) {
+      row[i] = a + b;
+      updateScore(row[i]);
+      row[i - 1] = 0;
+      //      break;
+    }
+  }
+  return row;
+}
+
+//  Drowing part  
+
+function updateCanvas() {
+  background(255);
+  drawGrid();
+  updateScore(0);
+  //  document.querySelector('#score').value = score;
+  //  select('#score').html(score);
+}
+
+function drawGrid() {
+  let w = 100;
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      noFill();
+      strokeWeight(2);
+      stroke(0);
+      rect(i * w, j * w, w, w);
+      let val = grid[i][j];
+      if (grid[i][j] !== 0) {
+        textAlign(CENTER, CENTER);
+        let s="" + val;
+        let len = s.length - 1;
+        let sizes = [64,64,48,32];
+        fill(0);
+        noStroke();
+        textSize(sizes[len]);
+        text(val, i * w + w / 2, j * w + w / 2);
+      }
+    }
+  }
+}
+
+function isGameOver() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (grid[i][j] == 0)
+        return false;
+      if (j!==3 && grid[i][j]==grid[i][j+1])
+        return false;
+      if (i!==3 && grid[i][j]==grid[i+1][j])
+        return false;
+    }
+  }
+  return true;
+}
